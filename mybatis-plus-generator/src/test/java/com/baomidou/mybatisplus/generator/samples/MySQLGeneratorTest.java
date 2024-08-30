ace5947a6df6227e987fb10b2baa111c78d74811
wangys34 <wangys34>
2024-08-30 16:04:55 +0800

    1.修改文件内容

--- mybatis-plus-generator/src/test/java/com/baomidou/mybatisplus/generator/samples/MySQLGeneratorTest.java
+++ mybatis-plus-generator/src/test/java/com/baomidou/mybatisplus/generator/samples/MySQLGeneratorTest.java
@@ -17,7 +17,7 @@ public class MySQLGeneratorTest extends BaseGeneratorTest {
      * 数据源配置
      */
     private static final DataSourceConfig DATA_SOURCE_CONFIG = new DataSourceConfig
-        .Builder("jdbc:mysql://192.168.33.24:3307/mos_safety_control?characterEncoding=utf8&useSSL=false&serverTimezone=Asia/Shanghai", "root", "123456")
+        .Builder("jdbc:mysql://192.168.33.24:3307/mos?characterEncoding=utf8&useSSL=false&serverTimezone=Asia/Shanghai", "root", "123456")
 //        .Builder("jdbc:mysql://192.168.33.116:3306/mos_dispatch?characterEncoding=utf8&useSSL=false&serverTimezone=Asia/Shanghai", "root", "123qwe")
         .schema("")
         .build();
@@ -25,7 +25,7 @@ public class MySQLGeneratorTest extends BaseGeneratorTest {
     @Test
     public void testSimple() {
         AutoGenerator generator = new AutoGenerator(DATA_SOURCE_CONFIG);
-        generator.strategy(strategyConfig().addInclude("safety_control_water_record").
+        generator.strategy(strategyConfig().addInclude("dispatch_task_pause_record").
             entityBuilder().enableLombok().addIgnoreColumns("id","create_user_id","create_user_name","update_user_name","update_user_id","create_time","update_time","delete_flag").enableChainModel()
             .mapperBuilder().enableBaseColumnList().enableBaseColumnList().build());
         generator.global(globalConfig()
